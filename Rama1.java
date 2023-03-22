8b606fadb1d124cb6519f378b580ba8ee92df47a
Miguel853835 <115725012+Miguel853835@users.noreply.github.com>
2023-03-22 17:28:38 +0100

    Add files via upload

---
+++ Rama1.java
@@ -0,0 +1,10 @@
+package paqueteRama1;
+
+public class Rama1 {
+
+	public static void main(String[] args) {
+		System.out.println("Código mensjae por pantalla creado para rama 1 del repositorio");
+
+	}
+
+}
